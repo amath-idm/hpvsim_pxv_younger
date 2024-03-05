9fc0bcddce186fcd96b5ebe1a40c8d8c001ebc9a
Robyn Stuart <robyna.s@gmail.com>
2024-03-05 13:53:45 +1100

    add version

--- version.py
+++ version.py
@@ -5,5 +5,5 @@ Version and license information.
 __all__ = ['__version__', '__versiondate__', '__license__']
 
 __version__ = '0.1.0'
-__versiondate__ = '2022-12-12'
-__license__ = f'HPVsim HPV Therapeutic Vaccine Analyses {__version__} ({__versiondate__}) — © 2022 by IDM'
+__versiondate__ = '2024-03-05'
+__license__ = f'HPVsim infant vaccination analyses {__version__} ({__versiondate__}) — © 2024 by IDM'
